99f3062152a46869cfba32ba157e8a76bd5f8157
unknown <henrique.c.ladeira@gmail.com>
2020-12-05 17:00:53 -0300

    feats and style

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './header.styles.scss'
 
 export const Header = () => (
-    <div className='header'> PokeAPI</div>
+    <div className='header'> PokeAPI in React </div>
 );
--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -2,15 +2,17 @@ import React, {useState} from 'react';
 import './sidebar.styles.scss'
 
 export const SideBar = ({pokemon}) => {
-    const [filter, setFilter] = useState('');
+    const [filterText, setFilterText] = useState('');
 
-    const search = (event) => setFilter(event.target.value);
+    const search = (event) => setFilterText(event.target.value);
 
     return (
     <div className='sideBar'>
         <input onChange={search} placeholder='search'/>
         {/* <div className='rectangle'/> */}
-        {pokemon.filter(pokemon => pokemon.name.toUpperCase().includes(filter.toUpperCase())).map(pokemon => (
+        {pokemon
+            .filter(pokemon => pokemon.name.toUpperCase().includes(filterText.toUpperCase()))
+            .map(pokemon => (
             <div key={pokemon.id} ><img src={pokemon.sprite} /> <span>{pokemon.name}</span></div>
         ))}
     </div>
