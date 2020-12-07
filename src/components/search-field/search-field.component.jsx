5757305ad5502d27ff91b21ed8b5288f2e9cfb20
unknown <henrique.c.ladeira@gmail.com>
2020-12-06 22:34:07 -0300

    style for SearchField

--- src/components/search-field/search-field.component.jsx
+++ src/components/search-field/search-field.component.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setSearchField } from '../../redux/search-field/search-field.actions'
 
+import './search-field.styles.scss'
+
 const SearchField = ({setSearchField}) => {
 
     const search = (event) =>
         setSearchField(event.target.value.toUpperCase());
     
     return (
-    <input 
+    <input class='searchField'
         onChange={search} 
         placeholder='Search for pokemon'/>
 )};
