f911065779ec6994b78753c8049eee0be52430ee
Henrique <henrique.c.ladeira@gmail.com>
2021-06-20 19:08:33 -0300

    ✨ feat: add hook for API call

---
+++ src/utils/api/hooks/usePokemon.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import api from '../index';
+
+export const usePokemon = () => {
+  const [cardList, setCardList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    api.get('pokemon?limit=151')
+      .then((response) => {
+        const dataList = response.data.results;
+        const cardList = dataList.map((data) => {
+          const id = data.url.split('/')[6];
+          return {
+            id,
+            name: data.name,
+            img: `https://pokeres.bastionbot.org/images/pokemon/${id}.png`,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+          };
+        });
+        setCardList(cardList);
+        setLoading(false);
+        setError(false);
+      })
+      .catch(() => {
+        setCardList([]);
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  return [cardList, loading, error];
+};
